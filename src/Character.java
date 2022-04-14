8d55290d13c199cdd8eac70dce5940a16d5ac1f1
theomass3 <theomass3@gmail.com>
2022-04-14 09:30:53 -0700

    Changed from Teacher guesser to Character guesser.

---
+++ src/Character.java
@@ -0,0 +1,10 @@
+public class Character {
+
+    private boolean isStem, isMale, teachesAP;
+
+    public Character(boolean isStem, boolean isMale, boolean teachesAP) {
+        this.isStem = isStem;
+        this.isMale = isMale;
+        this.teachesAP = teachesAP;
+    }
+}
