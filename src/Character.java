acd0f7d02429e7c94bc45e46cbbf8569f8ae0347
theomass3 <theomass3@gmail.com>
2022-04-14 09:34:35 -0700

    Apr 14, 2022

--- src/Character.java
+++ src/Character.java
@@ -2,7 +2,7 @@ public class Character {
 
     private boolean isStem, isMale, teachesAP;
 
-    public Character(boolean isStem, boolean isMale, boolean teachesAP) {
+    public Character(boolean alive_ROTJ, boolean isHuman, boolean isDroid) {
         this.isStem = isStem;
         this.isMale = isMale;
         this.teachesAP = teachesAP;
