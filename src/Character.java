3379fc5fa99a3d8b71ac125b0b08e646c79bf1f1
Luke Zanuck <lzanuck22@windwardschool.org>
2022-04-19 13:30:40 -0700

    Changed Character class, created initial characters

--- src/Character.java
+++ src/Character.java
@@ -1,63 +1,23 @@
-public class Character{
-    private String name;
-    private boolean isAlive, isHuman, isDroid, hasForce, inANH, inESB, inROTJ, isMale, speaksEnglish, isGoodGuy;
+public class Character {
+    private String name, path;
 
-    public Character(String name, boolean isAlive, boolean isHuman, boolean isDroid, boolean hasForce, boolean inANH, boolean inESB, boolean inROTJ, boolean isMale, boolean speaksEnglish, boolean isGoodGuy) {
+    public Character(String name, String path) {
         this.name = name;
-        this.isAlive = isAlive;
-        this.isHuman = isHuman;
-        this.isDroid = isDroid;
-        this.hasForce = hasForce;
-        this.inANH = inANH;
-        this.inESB = inESB;
-        this.inROTJ = inROTJ;
-        this.isMale = isMale;
-        this.speaksEnglish = speaksEnglish;
-        this.isGoodGuy = isGoodGuy;
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + path;
     }
 
     public String getName() {
         return name;
     }
 
-    public boolean isAlive() {
-        return isAlive;
+    public String getPath() {
+        return path;
     }
 
-    public boolean isHuman() {
-        return isHuman;
-    }
-
-    public boolean isDroid() {
-        return isDroid;
-    }
-
-    public boolean hasForce() {
-        return hasForce;
-    }
-
-    public boolean isInANH() {
-        return inANH;
-    }
-
-    public boolean isInESB() {
-        return inESB;
-    }
-
-    public boolean isInROTJ() {
-        return inROTJ;
-    }
-
-    public boolean isMale() {
-        return isMale;
-    }
-
-    public boolean speaksEnglish() {
-        return speaksEnglish;
-    }
-
-    public boolean isGoodGuy() {
-        return isGoodGuy;
-    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+    private final static int NUM_CHARACTERS = 20;
+
+    public static Character[] createCharacters() {
+
+        Character[] characters = new Character[NUM_CHARACTERS];
+
+        Scanner list = null;
+        try {
+            list = new Scanner(new File("resources/characters"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int i = 0;
+        while (list != null && list.hasNextLine()) {
+            String str = list.nextLine();
+            String[] data = str.split(":");
+            characters[i] = new Character(data[0], data[1]);
+            i++;
+        }
+
+        return characters;
+    }
+
     public static void main(String[] args) {
-        Character c1 = new Character("Anakin Skywalker",false,true,false,true,false,false,true,true,true,true);
-        Character c2 = new Character("Darth Vader",false,true,false,true,true,true,true,true,true,false);
-        Character c3 = new Character("Palpatine",true,true,false,true,false,true,true,true,true,false);
+
     }
 }
